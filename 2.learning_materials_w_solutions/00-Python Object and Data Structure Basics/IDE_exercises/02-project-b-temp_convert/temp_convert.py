f93593a82a226746b11a66c364821620d6d1e2b9
branhoff <branhoff2@gmail.com>
2020-10-21 13:43:08 -0700

    switched 'learning_materials' folder with '..._w_solutions' folder

--- 2.learning_materials_w_solutions/00-Python Object and Data Structure Basics/IDE_exercises/02-project-b-temp_convert/temp_convert.py
+++ 2.learning_materials_w_solutions/00-Python Object and Data Structure Basics/IDE_exercises/02-project-b-temp_convert/temp_convert.py
@@ -1,7 +1,13 @@
-# Author: 
-# Date: 
+# Author: Brandon Hoffman
+# Date: 09/24/2020
 # Description: Takes in a float value in Celsius and converts it to an equivalent
 #              value in Fahrenheit
 
-# Start coding here
+print("Please enter a Celsius temperature.")
+
+c_temp = float(input())
+f_temp = (9/5) * c_temp + 32
+
+print("The equivalent Fahrenheit temperature is:")
+print(f_temp)
 
