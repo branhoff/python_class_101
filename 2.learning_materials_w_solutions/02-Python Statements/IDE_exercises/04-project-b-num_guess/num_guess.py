b22877bf1fd2a072e23c1204e55202669dad939a
branhoff <branhoff2@gmail.com>
2020-10-22 06:54:54 -0700

    updated ppt naming conventions and added Strings. Corrected 'num_guess' IDE project

--- 2.learning_materials_w_solutions/02-Python Statements/IDE_exercises/04-project-b-num_guess/num_guess.py
+++ 2.learning_materials_w_solutions/02-Python Statements/IDE_exercises/04-project-b-num_guess/num_guess.py
@@ -9,18 +9,19 @@ solution = int(input())
 print("Enter your guess.")
 guess = int(input())
 
-number_of_guesses = 1
+if guess == solution:
+    print("You guessed it in 1 try.")
+else:
+    number_of_guesses = 1
+    
+    while guess != solution:
+        if guess > solution:
+            print("Too high - try again:")
+            guess = int(input())
+        if guess < solution:
+            print("Too low - try again:")
+            guess = int(input())
 
-while guess != solution:
-    if guess > solution:
-        print("Too high - try again:")
-        guess = int(input())
-    if guess < solution:
-        print("Too low - try again:")
-        guess = int(input())
+        number_of_guesses += 1
 
-    number_of_guesses += 1
-
-number_of_guesses +=1
-
-print("You guessed it in", number_of_guesses, "tries.")
+    print("You guessed it in", number_of_guesses, "tries.")
