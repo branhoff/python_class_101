ccd135b88c064318e3f24ecf9881a082c18f0a92
branhoff <branhoff2@gmail.com>
2021-05-15 10:32:49 -0700

    moved solution to solutions directory

--- IDE_exercises/project-1/my_color_sol.py
+++
@@ -1,7 +0,0 @@
-# Author: Brandon Hoffman
-# Date: 04/15/1865
-# Description: Asks the user for their favorite color and then
-#              prints out a statement with the variable
-
-fave_color = input("Please enter your favorite color: ")
-print("Your favorite color is  " + fave_color + ".")
--- IDE_exercises/project-6b/add_surname_sol.py
+++
@@ -1,9 +0,0 @@
-# Author: Brandon Hoffman
-# Date: 10/25/2020
-# Description: create a function that takes a list of names and stores any value that starts with the letter 'K' in a new list concatenated also with the string ' Kardashian'
-
-def add_surname(name_list):
-    """
-    takes a list of names and stores any value that starts with the letter 'K' in a new list concatenated also with the string ' Kardashian'
-    """
-    return [name + " Kardashian" for name in name_list if name[0] == "K"]
