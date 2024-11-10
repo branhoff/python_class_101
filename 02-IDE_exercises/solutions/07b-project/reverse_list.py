76eb8d94f872cd6d50dfe0b6dbfb21361832569b
Luis Mercado <lmmercado009@gmail.com>
2024-11-09 19:34:06 -0600

    improved comments in 07b project solution

--- 02-IDE_exercises/solutions/07b-project/reverse_list.py
+++ 02-IDE_exercises/solutions/07b-project/reverse_list.py
@@ -1,6 +1,6 @@
-#Author: Luis Mercado
-#Date: 10/24/2024
-#Description: Function that mutates a list of numbers by reversing its order without slicing
+# Author: Luis Mercado
+# Date: 10/24/2024
+# Description: Function that mutates a list of numbers by reversing its order without slicing
 
 
 def reverse_list(num_list: list[int | float]) -> None:
@@ -9,14 +9,14 @@ def reverse_list(num_list: list[int | float]) -> None:
     without using any slicing (no return value)
     """
     
-    #define lower limit of desired index range which is the same as upper limit of starting list argument
+    # Define lower limit of desired index range which is the same as upper limit of starting list argument
     low_lim: int = len(num_list)-1
     
-    #for range must use negative stride and go to -1 so that 0 index is included
+    # For range must use negative stride and go to -1 so that 0 index is included
     for num in range(low_lim, -1, -1):
-        num_list.append(num_list[num]) #adds currently indexed list value to end of list
-        del num_list[num] #deletes original iteration of currently indexed value from list
+        num_list.append(num_list[num]) # Adds currently indexed list value to end of list
+        del num_list[num] # Deletes original iteration of currently indexed value from list
 
-    #no return value, but by using .append and del[], the original num_list argument has iteratively had its last
-    #value duplicated to the end and then had the original occurrence of it deleted so that the list is now
-    #effectively in the opposite order
+    # No return value, but by using .append and del[], the original num_list argument has iteratively had its last
+    # value duplicated to the end and then had the original occurrence of it deleted so that the list is now
+    # effectively in the opposite order
